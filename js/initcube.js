4e803c6cf60579186abe809cb699e7a9aa7c8bba
ltaillefer1 <ltaillefer@btslivh.eu>
2020-03-25 16:35:03 +0100

    rajout de la variable manquant

--- js/initcube.js
+++ js/initcube.js
@@ -11,8 +11,7 @@ $( document ).ready(function(){      // lancé quand le DOM est initialisé (pr�
 	//à chaque fois que la valeur du slider change, on modifie la couleur de fond de la barre
 	//en fonction de la valeur du slider.
 	    var source = new EventSource("../cgi-bin/cubeEventServer.cgi");
-   			 source.addEventListener("matrice" = function(event) 
- 	 		{
+   			 source.addEventListener("matrice", function(event) {
         	var obj = JSON.parse (event.data);
         	document.getElementById("temp").innerHTML = obj.temp;
         	//document.getElementById("matrice").innerHTML = obj.matrice;
