25a25b6d268aee583fba3af07afca018ece072b9
ltaillefer1 <ltaillefer@btslivh.eu>
2020-03-26 15:38:39 +0100

    remove class initcuve

--- js/sketch.js
+++ js/sketch.js
@@ -1,12 +1,30 @@
-var initcube;
+var camera;
+var matrice
 
 function setup() {
     createCanvas(600, 600);
 
-    initcube = new Initcube2();
-
+    camera = new CCamera();
+    matrice = new CMatrice2(camera);
 }
 
 function draw() {
-    initcube.update();
+    update();
+}
+
+update() {
+    $(document).ready(function() { // lancé quand le DOM est initialisé (prêt)  
+
+        //à chaque fois que la valeur du slider change, on modifie la couleur de fond de la barre
+        //en fonction de la valeur du slider.
+        var source = new EventSource("../cgi-bin/cubeEventServer.cgi");
+        source.addEventListener("matrice", function(event) {
+            var obj = JSON.parse(event.data);
+            document.getElementById("temp").innerHTML = obj.temp;
+            //document.getElementById("matrice").innerHTML = obj.matrice;
+            camera.setPixel(obj.matrice);
+            matrice.update();
+            matrice.show();
+        });
+    });
 }
