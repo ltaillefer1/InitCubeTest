983f99fba2428e4a2299c99f8dc83ce9ccaa5815
ltaillefer1 <ltaillefer@btslivh.eu>
2020-03-26 15:35:25 +0100

    test

--- js/CMatrice2.js
+++ js/CMatrice2.js
@@ -13,7 +13,7 @@ class CMatrice2 {
         for (let i = 0; i < this.colonne; i++) {
             this.tab[i] = [];
             for (let j = 0; j < this.ligne; j++) {
-                this.tab[i][j] = new Square(this.x, this.y, this.sqWidth, this.camera.getPixel(this.numPixel));
+                this.tab[i][j] = new Square(this.x, this.y, this.sqWidth, this.camera.ExGetPixel(this.numPixel));
                 this.x += this.sqWidth + 3;
                 this.numPixel++;
             }
@@ -34,7 +34,7 @@ class CMatrice2 {
     update(){
     	for (let i = 0; i < this.colonne; i++) {
             for (let j = 0; j < this.ligne; j++) {
-                this.tab[i][j].setSquareTemp(this.camera.getPixel(this.numPixel));
+                this.tab[i][j].setSquareTemp(this.camera.ExGetPixel(this.numPixel));
                 this.numPixel++;
             }
         }
