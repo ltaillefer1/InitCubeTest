4a0f5ec2d9b782535aeef7aa73714c126f44afd8
ltaillefer1 <ltaillefer@btslivh.eu>
2020-03-26 16:57:42 +0100

    grosse modif

--- cgi-bin/eventPrincipale.cpp
+++ cgi-bin/eventPrincipale.cpp
@@ -18,7 +18,7 @@ int main(){
 			cout << "Event: mesure\r\n\r\n";
 			cout << "data : "<<endl;*/			
 		
-			cout << "event: matrice\r\n\r\n";
+			cout << "event: matrice\r\n";
 			for (int i=0 ; i<10;i++)
        		{
                 cout << "data:" << "{\"temp\":"<<number++<<",\"matrice\":["<<number++<<","<<number+2<<","<<number+3<<","<<number+4<<","<<number+5<<","<<number+6<<","<<number+7<<","<<number+8<<","<<number++<<","<<number+2<<","<<number+3<<","<<number+4<<","<<number+5<<","<<number+6<<","<<number+7<<","<<number+8<<","<<number++<<","<<number+2<<","<<number+3<<","<<number+4<<","<<number-5<<","<<number+6<<","<<number+7<<","<<number+8<<","<<number++<<","<<number+2<<","<<number+8<<","<<number+10<<","<<number+15<<","<<number+6<<","<<number+7<<","<<number+8<<","<<number++<<","<<number+2<<","<<number+3<<","<<number+4<<","<<number+5<<","<<number+6<<","<<number+7<<","<<number+8<<","<<number++<<","<<number+2<<","<<number+3<<","<<number+4<<","<<number-5<<","<<number+6<<","<<number+7<<","<<number+8<<","<<number++<<","<<number+2<<","<<number+3<<","<<number+4<<","<<number+5<<","<<number+6<<","<<number+7<<","<<number+8<<","<<number++<<","<<number+2<<","<<number+3<<","<<number+4<<","<<number+5<<","<<number+6<<","<<number+7<<","<<number+8<<"]}" "\r\n\r\n" <<endl;
@@ -110,7 +110,7 @@ int main(){
 			<<"\"cameraIR\":"
 			<<rand()%2
 
-     << "}";
+     << "}"<<"\r\n\r\n";
 
 	return 0;		
 }
