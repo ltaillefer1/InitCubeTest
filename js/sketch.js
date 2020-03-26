becfa439eb644b024d70e4e26b1b45d14dcc9d95
ltaillefer1 <ltaillefer@btslivh.eu>
2020-03-26 16:28:37 +0100

    test

--- js/sketch.js
+++ js/sketch.js
@@ -1,25 +1,37 @@
 var camerA;
 var matrice;
+var nbFrame;
 
-function setup(){
-	createCanvas(600, 600);
+function setup() {
+    createCanvas(600, 600);
+    frameRate(30);
+    nbFrame = 0;
 
-	camerA = new CCamera();
-	matrice = new CMatrice2(camerA);
+    camerA = new CCamera();
+    matrice = new CMatrice2(camerA);
 }
 
+function draw() {
+    matrice.show();
+    if(frameCount == nbFrame + 30){
+    	update();
+    	nbFrame = frameCount;
+    }
+}
 
-$(document).ready(function() { // lancé quand le DOM est initialisé (prêt)  
+function update() {
+    $(document).ready(function() { // lancé quand le DOM est initialisé (prêt)  
 
-    //à chaque fois que la valeur du slider change, on modifie la couleur de fond de la barre
-    //en fonction de la valeur du slider.
-    var source = new EventSource("../cgi-bin/cubeEventServer.cgi");
-    source.addEventListener("matrice", function(event) {
-        var obj = JSON.parse(event.data);
-        document.getElementById("temp").innerHTML = obj.temp;
-        //document.getElementById("matrice").innerHTML = obj.matrice;
-        test.setPixel(obj.matrice);
-        matrice.update();
-        matrice.show();
+        //à chaque fois que la valeur du slider change, on modifie la couleur de fond de la barre
+        //en fonction de la valeur du slider.
+        var source = new EventSource("../cgi-bin/cubeEventServer.cgi");
+        source.addEventListener("matrice", function(event) {
+            var obj = JSON.parse(event.data);
+            document.getElementById("temp").innerHTML = obj.temp;
+            //document.getElementById("matrice").innerHTML = obj.matrice;
+            test.setPixel(obj.matrice);
+            matrice.update();
+            matrice.show();
+        });
     });
-});
+}
